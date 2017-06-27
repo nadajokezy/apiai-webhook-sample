f6a81557494cea4dccd7f232a143e45ec566b29f
nadajokezy <nadajokezyy@gmail.com>
2017-06-27 14:47:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     var a = parseFloat(req.body.result.parameters.firstNum);
     var b = parseFloat(req.body.result.parameters.secondNum);
+    var data;
     if(req.body.result.action === 'searchA'){
         speech = "https://www.lamsao.com/tim-kiem.html?q=" + req.body.result.parameters.article.replace(/ /g,"%20");
     }
@@ -30,9 +31,27 @@ restService.post('/echo', function(req, res) {
             speech = "Dở hơi vcl. Éo ai đi chia cho 0. Về học lớp 1 đi.";
         }
     }
+    if(req.body.result.action === 'mood'){
+        var degree = req.body.result.parameters.feeling.degree;
+        var mood = req.body.result.parameters.feeling.mood;
+        speech = 'Hệ thống phân tích bạn đang '+mood+' ở mức '+degree;
+        if(mood === 'vui'){
+            data = {
+                "facebook": {
+                    "attachment": {
+                        "type": "audio",
+                        "payload": {
+                        "url": "https://upload.wikimedia.org/wikipedia/en/a/a3/Happy_%28Pharrell_Williams_song_-_sample%29.ogg"
+                        }
+                    }
+                }
+            }
+        }
+    }
     return res.json({
         speech: speech,
         displayText: speech,
+        data: data,
         source: 'math-test-by-huy'
     });
 });
