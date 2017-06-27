9500fba35d65ff80f14cb10fb03d2bbd6d6ee268
nadajokezy <nadajokezyy@gmail.com>
2017-06-27 12:07:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const opn = require('opn');
 const restService = express();
 
 restService.use(bodyParser.urlencoded({
