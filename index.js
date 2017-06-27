91054338d5a8755611107de6358eb97e99c09634
nadajokezy <nadajokezyy@gmail.com>
2017-06-27 10:09:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,16 +12,16 @@ restService.use(bodyParser.json());
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     if(req.body.result.action === 'plus'){
-        speech = req.req.body.result.parameters.firstNum + req.req.body.result.parameters.secondNum;
+        speech = req.body.result.parameters.firstNum + req.body.result.parameters.secondNum;
     }
     if(req.body.result.action === 'minus'){
-        speech = req.req.body.result.parameters.firstNum - req.req.body.result.parameters.secondNum;
+        speech = req.body.result.parameters.firstNum - req.body.result.parameters.secondNum;
     }
     if(req.body.result.action === 'multiply'){
-        speech = req.req.body.result.parameters.firstNum * req.req.body.result.parameters.secondNum;
+        speech = req.body.result.parameters.firstNum * req.body.result.parameters.secondNum;
     }
     if(req.body.result.action === 'divide'){
-        speech = req.req.body.result.parameters.firstNum / req.req.body.result.parameters.secondNum;
+        speech = req.body.result.parameters.firstNum / req.body.result.parameters.secondNum;
     }
     return res.json({
         speech: speech,
