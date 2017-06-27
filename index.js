04a89ad3975a43129f4171b1a0925f48e6e99a74
nadajokezy <nadajokezyy@gmail.com>
2017-06-27 10:11:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,17 +11,19 @@ restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    var a = parseFloat(req.body.result.parameters.firstNum);
+    var b = parseFloat(req.body.result.parameters.secondNum);
     if(req.body.result.action === 'plus'){
-        speech = req.body.result.parameters.firstNum + req.body.result.parameters.secondNum;
+        a+b;
     }
     if(req.body.result.action === 'minus'){
-        speech = req.body.result.parameters.firstNum - req.body.result.parameters.secondNum;
+        a-b;
     }
     if(req.body.result.action === 'multiply'){
-        speech = req.body.result.parameters.firstNum * req.body.result.parameters.secondNum;
+        a*b;
     }
     if(req.body.result.action === 'divide'){
-        speech = req.body.result.parameters.firstNum / req.body.result.parameters.secondNum;
+        a/b;
     }
     return res.json({
         speech: speech,
