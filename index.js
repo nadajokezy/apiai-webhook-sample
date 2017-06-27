00f3032b40379101b4987c92dc4c6bf2a643ed56
nadajokezy <nadajokezyy@gmail.com>
2017-06-27 15:44:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,36 +37,37 @@ restService.post('/echo', function(req, res) {
         speech = 'Hệ thống phân tích bạn đang '+mood+' ở mức '+degree;
         if(mood === 'vui'){
             data = "attachment":{
-      "type":"template",
-      "payload":{
-        "template_type":"generic",
-        "elements":[
-           {
-            "title":"Welcome to Peter\'s Hats",
-            "image_url":"https://petersfancybrownhats.com/company_image.png",
-            "subtitle":"We\'ve got the right hat for everyone.",
-            "default_action": {
-              "type": "web_url",
-              "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
-              "messenger_extensions": true,
-              "webview_height_ratio": "tall",
-              "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-            },
-            "buttons":[
-              {
-                "type":"web_url",
-                "url":"https://petersfancybrownhats.com",
-                "title":"View Website"
-              },{
-                "type":"postback",
-                "title":"Start Chatting",
-                "payload":"DEVELOPER_DEFINED_PAYLOAD"
-              }              
-            ]      
-          }
-        ]
-      }
-    }
+                      "type":"template",
+                      "payload":{
+                        "template_type":"generic",
+                        "elements":[
+                           {
+                            "title":"Welcome to Peter\'s Hats",
+                            "image_url":"https://petersfancybrownhats.com/company_image.png",
+                            "subtitle":"We\'ve got the right hat for everyone.",
+                            "default_action": {
+                              "type": "web_url",
+                              "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
+                              "messenger_extensions": true,
+                              "webview_height_ratio": "tall",
+                              "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                            },
+                            "buttons":[
+                              {
+                                "type":"web_url",
+                                "url":"https://petersfancybrownhats.com",
+                                "title":"View Website"
+                              },{
+                                "type":"postback",
+                                "title":"Start Chatting",
+                                "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                              }              
+                            ]      
+                          }
+                        ]
+                      }
+                    }
+        }
     return res.json({
         speech: speech,
         displayText: speech,
