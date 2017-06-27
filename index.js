a521f09e7658e3a56065632e2edddce33de82a2c
nadajokezy <nadajokezyy@gmail.com>
2017-06-27 10:04:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,58 +1,22 @@
-'use strict';
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
 const restService = express();
+
+restService.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 restService.use(bodyParser.json());
 
-restService.post('/webhook', function (req, res) {
-
-    console.log('hook request');
-
-    try {
-        var speech = 'empty speech';
-
-        if (req.body) {
-            var requestBody = req.body;
-
-            if (requestBody.result) {
-                speech = '';
-                if (requestBody.result.action === 'plus') {
-                    speech = requestBody.result.action.parameters.firstNum + requestBody.result.action.parameters.secondNum;
-                }
-                if (requestBody.result.action === 'minus') {
-                    speech = requestBody.result.action.parameters.firstNum - requestBody.result.action.parameters.secondNum;
-                }
-                if (requestBody.result.action === 'multiply') {
-                    speech = requestBody.result.action.parameters.firstNum * requestBody.result.action.parameters.secondNum;
-                }
-                if (requestBody.result.action === 'divide') {
-                    speech = requestBody.result.action.parameters.firstNum / requestBody.result.action.parameters.secondNum;
-                }
-            }
-        }
-
-        console.log('result: ', speech);
-
-        return res.json({
-            speech: speech,
-            displayText: speech,
-            source: 'math_api_by_huy'
-        });
-    } catch (err) {
-        console.error("Can't process request", err);
-
-        return res.status(400).json({
-            status: {
-                code: 400,
-                errorType: err.message
-            }
-        });
-    }
+restService.post('/echo', function(req, res) {
+    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    return res.json({
+        speech: speech,
+        displayText: speech,
+        source: 'webhook-echo-sample'
+    });
 });
-
-restService.listen((process.env.PORT || 5000), function () {
-    console.log("Server listening");
+restService.listen((process.env.PORT || 8000), function() {
+    console.log("Server up and listening");
 });
