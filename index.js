9f91aed6d93ae4f46c80418db158a7344bb5f491
nadajokezy <nadajokezyy@gmail.com>
2017-06-27 10:07:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,10 +11,22 @@ restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    if(req.body.result.action === 'plus'){
+        speech = req.req.body.result.parameters.firstNum + req.req.body.result.parameters.secondNum;
+    }
+    if(req.body.result.action === 'minus'){
+        speech = req.req.body.result.parameters.firstNum - req.req.body.result.parameters.secondNum;
+    }
+    if(req.body.result.action === 'multiply'){
+        speech = req.req.body.result.parameters.firstNum * req.req.body.result.parameters.secondNum;
+    }
+    if(req.body.result.action === 'divide'){
+        speech = req.req.body.result.parameters.firstNum / req.req.body.result.parameters.secondNum;
+    }
     return res.json({
         speech: speech,
         displayText: speech,
-        source: 'webhook-echo-sample'
+        source: 'math-test-by-huy'
     });
 });
 restService.listen((process.env.PORT || 8000), function() {
